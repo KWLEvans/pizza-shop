7bd028f9f0c906e638f95d5d5fba23db7d1094d2
Keith Evans <kwlevans@gmail.com>
2017-01-27 08:38:20 -0800

    add size selection and pricing

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,21 @@
 //Back-end
 /////////////////////////////////////////////
 
+var prices = {
+  small: 6,
+  medium: 10,
+  large: 14,
+  pizzilla: 32
+}
 
+function Pizza(size) {
+  this.size = size;
+}
 
-
+Pizza.prototype.price = function() {
+  var price = prices[this.size];
+  return price;
+}
 
 
 
@@ -14,7 +26,12 @@
 /////////////////////////////////////////////
 
 $(function() {
-  $("#order-pizza").click(function() {
-    $("#order-display").text("Yous gotta pizza!");
+  $("#order-form").submit(function(event) {
+    event.preventDefault();
+
+    var pizzaSize = $("#sizes input:checked").val();
+    var newPizza = new Pizza(pizzaSize);
+
+    $("#order-display").text("Yous gotta " + newPizza.size + " pizza! And it'll cost you $" + newPizza.price() + ".");
   });
 });
